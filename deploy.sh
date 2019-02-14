8c78420622170a6bc71cc91febf94e62896ce105
SebastianMeler <sebastianissimus@gmail.com>
2019-02-14 11:40:49 +0100

    added deployments scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t smeler/multi-client:latest -t smeler/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t smeler/multi-server:latest -t smeler/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t smeler/multi-worker:latest -t smeler/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push smeler/multi-client:latest
+docker push smeler/multi-server:latest
+docker push smeler/multi-worker:latest
+docker push smeler/multi-client:$SHA
+docker push smeler/multi-server:$SHA
+docker push smeler/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=smeler/multi-server:$SHA
+kubectl set image deployments/client-deployment server=smeler/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=smeler/multi-worker:$SHA
